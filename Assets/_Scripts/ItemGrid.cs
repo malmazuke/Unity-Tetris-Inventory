44e5d7d77254d2029d64151817469a55518e91c1
Mark Feaver <mark.feaver@gmail.com>
2025-06-04 21:05:29 +1000

    Allow grid size to be configurable

--- Assets/_Scripts/ItemGrid.cs
+++ Assets/_Scripts/ItemGrid.cs
@@ -2,14 +2,26 @@ using UnityEngine;
 
 public class ItemGrid : MonoBehaviour
 {
+    [SerializeField] private Vector2Int _gridSize = new Vector2Int(10, 10);
+
     private const float TileSizeWidth = 32f;
     private const float TileSizeHeight = 32f;
 
     private RectTransform _rectTransform;
 
+    private InventoryItem[,] _inventoryItemSlot;
+
     private void Start()
     {
         _rectTransform = GetComponent<RectTransform>();
+        InitialiseGrid(_gridSize.x, _gridSize.y);
+    }
+
+    private void InitialiseGrid(int width, int height)
+    {
+        _inventoryItemSlot = new InventoryItem[width, height];
+        var size = new Vector2(width * TileSizeWidth, height * TileSizeHeight);
+        _rectTransform.sizeDelta = size;
     }
 
     public Vector2Int GetTileGridPosition(Vector2 mousePosition)
